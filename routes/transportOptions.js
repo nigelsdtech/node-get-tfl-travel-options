87ad5baa7445b04eb2a4fa55d003bd34b1d36318
Nigel Stephen D'Souza <nigelsdtech@gmail.com>
2018-02-27 12:09:34 +0000

    Now reads train services from Darwin

--- routes/transportOptions.js
+++ routes/transportOptions.js
@@ -1,9 +1,10 @@
-var cfg     = require('config');
-var express = require('express');
-var log4js  = require('log4js');
-var router  = express.Router();
-var TflStop = require('../lib/TflStop.js');
-var Q       = require('q');
+var cfg       = require('config');
+var express   = require('express');
+var log4js    = require('log4js');
+var router    = express.Router();
+var TflStop   = require('../lib/TflStop.js');
+var TrainStop = require('../lib/TrainStop.js');
+var Q         = require('q');
 require('q-foreach')(Q);
 
 
@@ -15,14 +16,21 @@ log4js.configure(cfg.log.log4jsConfigs);
 var log = log4js.getLogger(cfg.log.appName);
 log.setLevel(cfg.log.level);
 
-var tflStops = []
 
-for (var i = 0; i < cfg.tflStops.length; i++) {
 
-  tflStops.push(new TflStop (cfg.tflStops[i]))
+var stops = []
+
+for (var i = 0; i < cfg.stops.length; i++) {
+
+  var stop = cfg.stops[i]
+
+  if (stop.infoService == "tfl") {
+    stops.push(new TflStop (stop))
+  } else if (stop.infoService == "train") {
+    stops.push(new TrainStop (stop))
+  }
 
 }
-
 
 
 /* GET tfl transport options */
@@ -30,19 +38,19 @@ router.get('/', function(req, res, next) {
 
   log.info('transportOptions BEGIN.')
 
-  Q.forEach(tflStops, function(tflStop) {
+  Q.forEach(stops, function(stop) {
 
-    var getArrivals = Q.nbind(tflStop.getArrivals, tflStop)
+    var getArrivals = Q.nbind(stop.getArrivals, stop)
     return Q.nfcall(getArrivals, null)
 
-  }).then (function (tflStops) {
+  }).then (function (stops) {
 
     var ret = ""
 
-    for (var i = 0; i < tflStops.length; i++) {
-      var arrivals = tflStops[i].getArrivalsString()
+    for (var i = 0; i < stops.length; i++) {
+      var arrivals = stops[i].getArrivalsString()
       log.info(arrivals)
-      ret += arrivals + " "
+      ret += arrivals + "\n"
     }
 
 
